e4f1778d6cfb8a23b87d3d4ca34df4de6e1d84d1
evalphobia <evalphobia@gmail.com>
2021-09-01 16:19:04 +0900

    [sqs] Fix 'XIsEmptyQueue' (#37)

--- sqs/client_xapi_attributes.go
+++ sqs/client_xapi_attributes.go
@@ -7,23 +7,26 @@ import (
 
 // XIsEmptyQueue checks if the queue does not have any message.
 func (svc *SQS) XIsEmptyQueue(ctx context.Context, queueURL string) (bool, error) {
+	attrList := []QueueAttributeName{
+		QueueAttributeNameApproximateNumberOfMessages,
+		QueueAttributeNameApproximateNumberOfMessagesDelayed,
+		QueueAttributeNameApproximateNumberOfMessagesNotVisible,
+	}
 	resp, err := svc.GetQueueAttributes(ctx, GetQueueAttributesRequest{
-		QueueURL: queueURL,
-		AttributeNames: []QueueAttributeName{
-			QueueAttributeNameApproximateNumberOfMessages,
-			QueueAttributeNameApproximateNumberOfMessagesDelayed,
-			QueueAttributeNameApproximateNumberOfMessagesNotVisible,
-		},
+		QueueURL:       queueURL,
+		AttributeNames: attrList,
 	})
 	if err != nil {
 		return false, err
 	}
 
-	// if v, ok := resp.Attributes[QueueAttributeNameApproximateNumberOfMessages]; ok {
-
-	// }
-	for _, v := range resp.Attributes {
-		num, err := strconv.Atoi(v)
+	result := resp.Attributes
+	for _, v := range attrList {
+		attr, ok := result[string(v)]
+		if !ok {
+			return false, nil
+		}
+		num, err := strconv.Atoi(attr)
 		if err != nil {
 			return false, err
 		}
